5c96a1e625e27af36e58bae9e2c9de8fd740374c
Kumegawa <lucaskumegawa222@gmail.com>
2025-05-21 14:00:31 -0300

    adicionando footer fixo com links institucionais

---
+++ app/views/partials/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="container">
+        <p>&copy; <?= date('Y') ?> Sistema de Gestão de Aprendizagem</p>
+        <p><a href="#">Termos</a> | <a href="#">Contato</a></p>
+    </div>
+</footer>
