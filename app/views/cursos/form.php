18094d592a51665084203f3002c5aa1e12dbd847
Kumegawa <lucaskumegawa222@gmail.com>
2025-05-21 12:28:12 -0300

    criando formulário para criação de cursos

---
+++ app/views/cursos/form.php
@@ -0,0 +1,5 @@
+<form method="POST" action="/cursos/store">
+    <input type="text" name="titulo" placeholder="Título">
+    <textarea name="descricao" placeholder="Descrição"></textarea>
+    <button type="submit">Salvar</button>
+</form>
