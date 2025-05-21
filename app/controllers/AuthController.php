d6a69ed9a3c9f66ebc1a2ff2c3be81352527cb6c
Kumegawa <lucaskumegawa222@gmail.com>
2025-05-21 12:24:18 -0300

    criando AuthController com login e verificação via password_verify

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+class AuthController {
+    public function login() {
+        require 'app/views/auth/login.php';
+    }
+
+    public function processLogin() {
+        global $pdo;
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        $usuarioModel = new Usuario();
+        $user = $usuarioModel->findByEmail($pdo, $email);
+
+        if ($user && password_verify($senha, $user['senha_hash'])) {
+            $_SESSION['usuario_id'] = $user['id'];
+            $_SESSION['perfil'] = $user['perfil'];
+            header('Location: /painel');
+        } else {
+            echo "Login inválido";
+        }
+    }
+}
