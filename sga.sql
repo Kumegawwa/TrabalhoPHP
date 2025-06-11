43f63f515af304f36ac53bc8cc694b8aff90a03d
Kumegawa <lucaskumegawa222@gmail.com>
2025-06-11 04:02:15 -0300

    Adicionando banco de dados teste para utilização do projeto

---
+++ sga.sql
@@ -0,0 +1,233 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 11/06/2025 às 09:01
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `sga`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `professor_id` int(11) NOT NULL,
+  `codigo_turma` varchar(10) DEFAULT NULL,
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `cursos`
+--
+
+INSERT INTO `cursos` (`id`, `titulo`, `descricao`, `professor_id`, `codigo_turma`, `data_criacao`) VALUES
+(3, 'Java', 'Aulas ', 1, 'MV2UQE', '2025-06-11 06:45:29');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `entregas_atividades`
+--
+
+CREATE TABLE `entregas_atividades` (
+  `id` int(11) NOT NULL,
+  `atividade_id` int(11) NOT NULL,
+  `aluno_id` int(11) NOT NULL,
+  `arquivo_entrega` varchar(255) DEFAULT NULL COMMENT 'Caminho para o arquivo',
+  `texto_entrega` text DEFAULT NULL COMMENT 'Resposta em texto',
+  `data_entrega` timestamp NOT NULL DEFAULT current_timestamp(),
+  `nota` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `inscricoes_cursos`
+--
+
+CREATE TABLE `inscricoes_cursos` (
+  `id` int(11) NOT NULL,
+  `aluno_id` int(11) NOT NULL,
+  `curso_id` int(11) NOT NULL,
+  `data_inscricao` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `materiais_atividades`
+--
+
+CREATE TABLE `materiais_atividades` (
+  `id` int(11) NOT NULL,
+  `curso_id` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `conteudo` text DEFAULT NULL,
+  `tipo` enum('material','atividade') NOT NULL,
+  `data_postagem` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `materiais_atividades`
+--
+
+INSERT INTO `materiais_atividades` (`id`, `curso_id`, `titulo`, `conteudo`, `tipo`, `data_postagem`) VALUES
+(3, 3, 'Lista 1', ' ', 'atividade', '2025-06-11 06:45:42');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha_hash` varchar(255) NOT NULL,
+  `perfil` enum('aluno','professor','admin') NOT NULL,
+  `cpf` varchar(14) NOT NULL COMMENT 'Formato com máscara: 123.456.789-00',
+  `data_nascimento` date NOT NULL,
+  `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha_hash`, `perfil`, `cpf`, `data_nascimento`, `data_cadastro`) VALUES
+(1, 'Professor Admin', 'prof@sga.com', '$2y$10$d4IDd3DL6fAbCrO4bMa/6ez2uzBX3DEhrGg523YGbO4A5HqNuyF6m', 'professor', '111.111.111-11', '1990-01-01', '2025-06-11 04:48:35'),
+(2, 'Aluno Teste', 'aluno@sga.com', '$2y$10$sn1m4vppYbv7bMyXZRWt2.XauteInZQ/yBdXSwzceYCevXpoeNDuq', 'aluno', '222.222.222-22', '2000-05-10', '2025-06-11 04:48:35'),
+(3, 'Martin lutero', 'teste@gmail.com', '$2y$10$RgSojU23VtVBQhTWLt1dqukFfVDLWi7Lo/fmm0gyjLx.913fCXAEK', 'aluno', '333.333.333-33', '2002-10-28', '2025-06-11 05:24:28');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `codigo_turma` (`codigo_turma`),
+  ADD KEY `professor_id` (`professor_id`);
+
+--
+-- Índices de tabela `entregas_atividades`
+--
+ALTER TABLE `entregas_atividades`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `atividade_id` (`atividade_id`),
+  ADD KEY `aluno_id` (`aluno_id`);
+
+--
+-- Índices de tabela `inscricoes_cursos`
+--
+ALTER TABLE `inscricoes_cursos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `aluno_curso_unique` (`aluno_id`,`curso_id`),
+  ADD KEY `curso_id` (`curso_id`);
+
+--
+-- Índices de tabela `materiais_atividades`
+--
+ALTER TABLE `materiais_atividades`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `curso_id` (`curso_id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `entregas_atividades`
+--
+ALTER TABLE `entregas_atividades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `inscricoes_cursos`
+--
+ALTER TABLE `inscricoes_cursos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `materiais_atividades`
+--
+ALTER TABLE `materiais_atividades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `cursos`
+--
+ALTER TABLE `cursos`
+  ADD CONSTRAINT `cursos_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `entregas_atividades`
+--
+ALTER TABLE `entregas_atividades`
+  ADD CONSTRAINT `entregas_atividades_ibfk_1` FOREIGN KEY (`atividade_id`) REFERENCES `materiais_atividades` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `entregas_atividades_ibfk_2` FOREIGN KEY (`aluno_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `inscricoes_cursos`
+--
+ALTER TABLE `inscricoes_cursos`
+  ADD CONSTRAINT `inscricoes_cursos_ibfk_1` FOREIGN KEY (`aluno_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `inscricoes_cursos_ibfk_2` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `materiais_atividades`
+--
+ALTER TABLE `materiais_atividades`
+  ADD CONSTRAINT `materiais_atividades_ibfk_1` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
