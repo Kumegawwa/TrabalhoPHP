40189c3348b1b8ca06cf2916edb20af7d9cfdc15
Kumegawa <lucaskumegawa222@gmail.com>
2025-06-11 02:09:43 -0300

    ajustando a view

--- app/views/site/home.php
+++ app/views/site/home.php
@@ -1,2 +1,6 @@
+<?php include __DIR__ . '/../partials/header.php'; ?>
+
 <h1>Bem-vindo ao SGA</h1>
 <p>Uma plataforma simples de aprendizagem.</p>
+
+<?php include __DIR__ . '/../partials/footer.php'; ?>
